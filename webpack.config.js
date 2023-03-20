3546d2e9d44c570b82ac3534fbd5802461f73d6a
e.boganov <jenyaz862@gmail.com>
2023-03-20 19:55:16 +0300

    init: init project

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let mode = "development"
+if (process.env.NODE_ENV === 'production'){
+  mode = 'production';
+}
+
+const devMode = mode === 'development';
+
+const devtool = devMode? 'source-map': undefined;
+
+console.log('mode', mode);
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: "index[contenthash].js",
+  },
+  mode: mode,
+  devtool: devtool,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader"
+      },
+      {
+        test: /\.(sc|sa|c)ss$/i,
+        use: [
+          devMode ? "style-loader": MiniCssExtractPlugin.loader,
+          "css-loader"
+        ],
+      }
+    ]
+  },
+  experiments: {
+    topLevelAwait: true
+  }
+}
