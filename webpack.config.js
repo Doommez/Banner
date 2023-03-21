1e4cdd426141facb8ad6c9e1787b2a14d358e826
e.boganov <jenyaz862@gmail.com>
2023-03-21 23:49:55 +0300

    fix: finaly fixes and deploy

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     filename: "index[contenthash].js",
+    assetModuleFilename: "assets/[hash][ext][query]",
   },
   mode: mode,
   devtool: devtool,
